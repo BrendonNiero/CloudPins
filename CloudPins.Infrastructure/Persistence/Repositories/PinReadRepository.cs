efc74a6f0513c06a6b6acfa54226ad8f69e4aa03
Brendon Berzins <brendonsoftwaredeveloper@gmail.com>
2026-01-14 21:56:43 -0300

    feat: implementando GetByIdAsync em Repository persistence

--- CloudPins.Infrastructure/Persistence/Repositories/PinReadRepository.cs
+++ CloudPins.Infrastructure/Persistence/Repositories/PinReadRepository.cs
@@ -1,8 +1,35 @@
 using CloudPins.Application.Common.Interfaces;
+using CloudPins.Application.Pins.GetById;
+using Microsoft.EntityFrameworkCore;
 
 namespace CloudPins.Infrastructure.Persistence.Repositories;
 
 public class PinReadRepository : IPinReadRepository
 {
     private readonly CloudPinsDbContext _context;
+
+    public PinReadRepository(CloudPinsDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PinDetailsDto?> GetByIdAsync(Guid pinId, CancellationToken ct)
+    {
+        return await _context.Pins
+        .AsNoTracking()
+        .Where(p => p.Id == pinId)
+        .Select(p => new PinDetailsDto
+        {
+            Id = p.Id,
+            BoardId = p.BoardId,
+            OwnerId = p.OwnerId,
+            ImageUrl = p.ImageUrl,
+            ThumbnailUrl = p.ThumbnailUrl,
+            Description = p.Description,
+            LikesCount = p.LikesCount,
+            TagIds = p.TagIds.ToList(),
+            CreatedAt = p.CreatedAt
+        })
+        .FirstOrDefaultAsync(ct);
+    }
 }
